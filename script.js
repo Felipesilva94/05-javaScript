6b159913718db97a2d756b391d03f298a3e30eba
Felipesilva94 <lippepunhal@gmail.com>
2022-06-22 23:32:41 -0300

    05 javaScript

---
+++ script.js
@@ -0,0 +1,51 @@
+
+ // Crie um array que receba 5 itens e exiba no console.
+
+ let tecnologia = ['javaScript', 'HTML', 'CSS', 'GIT', 'JAVA'];
+ console.log(tecnologia)
+ 
+console.log('')
+console.log('======================================')
+console.log('com metodo unshift')
+
+// Utilize um método para adicionar um nome ao inicio do array.
+tecnologia.unshift('REACT')
+console.log(tecnologia)
+
+console.log('')
+console.log('======================================')
+console.log('com metodo pop')
+
+// Utilize um método para remover o último nome do array.
+tecnologia.pop()
+console.log(tecnologia)
+
+console.log('')
+console.log('======================================')
+console.log('com metodo  push')
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+tecnologia.push('NODE.JS', 'PHP')
+console.log(tecnologia)
+
+console.log('')
+console.log('======================================')
+console.log('com metodo splice')
+
+// Utilize um método para remover o primeiro nome do array.
+tecnologia.splice(0, 1)
+console.log(tecnologia)
+console.log('')
+console.log('======================================')
+console.log('com metodo sort')
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
+console.log('')
+console.log('======================================')
+console.log('com for')
+
+
+       
